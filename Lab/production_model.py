14782292b7643b206423c3b7537332f5ea6fca2a
Mar-kav <97294723+Mar-kav@users.noreply.github.com>
2024-03-22 14:06:25 +0100

    Remove double scaling 	modified:   Lab/model_lgr.pkl 	modified:   Lab/predictions.csv 	modified:   Lab/production_model.py

--- Lab/production_model.py
+++ Lab/production_model.py
@@ -1,18 +1,15 @@
 import pandas as pd
 from joblib import load as ld
-from sklearn.preprocessing import StandardScaler
 
 test_data = pd.read_csv("Machine_Learning-Mary-Kavalionak\\Lab\\Data\\test_samples.csv", index_col= "id")
 
 model = ld('Machine_Learning-Mary-Kavalionak\\Lab\\model_lgr.pkl')
 
 X_test = test_data.drop("cardio", axis = 1)
-scaler = StandardScaler()
-scaled_X = scaler.fit_transform(X_test)
 
-probabilities = model.predict_proba(scaled_X)
+probabilities = model.predict_proba(X_test)
 
-predictions = model.predict(scaled_X)
+predictions = model.predict(X_test)
 
 results = pd.DataFrame({
     'probability class 0': probabilities[:, 0],
